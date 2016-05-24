eb319c3e49d2bcb44d15ef8a551fc4117011f76d
AlexisBattagli <battagli.alexis@hotmail.fr>
2016-05-24 14:58:12 +0200

    Ajout de la view pour créer un Objet (on en peut aps supprimer un Objet)

--- view/phtml/gst_objet.php
+++ view/phtml/gst_objet.php
@@ -1,17 +1,16 @@
 <!DOCTYPE html>
+
 <!--
-- Page d'ajout de Lieux du site web Gestionnaire de Compte version 2.0 
+- Page d'ajout d'Objet du site web Gestionnaire de Compte version 2.0 
 - 
 - @author Alexis BATTAGLI
 - @version 0.1
 - 
-- Permet d'afficher un formulaire pour ajouter un lieux
-- Doit permettre de donnée un nom de ville, et un nom de pays.
+- Permet d'afficher un formulaire pour ajouter un objet
+- Doit permettre de donnée un label, et une description.
 -->
 
-<?php
-require_once($_SERVER['DOCUMENT_ROOT'] . '/model/DAL/LieuDAL.php');
-?>
+<?php require_once($_SERVER['DOCUMENT_ROOT'] . '/model/DAL/ObjetDAL.php'); ?>
 
 <html lang="fr">
     <head>
@@ -60,7 +59,6 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/model/DAL/LieuDAL.php');
             blockquote {
                 background-color:#fffeee;
                 border-radius: 12px;
-                //box-shadow: 3px 3px 6px #8c8c8c;
                 border: 2px #eee dotted; 
             }
         </style>
@@ -88,9 +86,77 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/model/DAL/LieuDAL.php');
                     </nav>
                 </div>
             </div>
+        
+            <div class="row">
+                <div class="col-lg-12">
+                    <form action=<?php $_SERVER['DOCUMENT_ROOT'] ?>"/controller/page/add_objet.php" method="POST">
+                        <legend>Formulaire d'ajout d'un Objet</legend>
+                        
+                        <!-- Label de l'objet-->
+                        <div class="col-lg-2">
+                            <label for="label" class="control-label">Label* : </label>
+                            <input type="text" maxlength="30" name="label" class="form-control" id="label" placeholder="Ex: Salaire" required="required">
+                        </div>
+
+                        <!-- Description de l'objet-->
+                        <div class="col-lg-4">
+                            <label for="description" class="control-label">Description : </label>
+                            <input type="text" maxlength="150" name="description" class="form-control" id="description" placeholder="Ex: Permet d'identifier toutes les ES liées à un salaire">
+                        </div>
+
+                        <!-- Bouton de Validation-->
+                        <div class="col-lg-2">
+                            </br>
+                            <input type="submit" value="Ajouter cet Objet" class="btn btn-success btn-block"/>
+                        </div>
+                    </form>
+
+                    <div class="col-lg-2">
+                        </br>
+                        <a href=<?php $_SERVER['DOCUMENT_ROOT'] ?>"/view/phtml/home.php" class="btn btn-danger btn-block">Annuler</a>
+                    </div>
+                </div>
+            </div>
+            
+            <br>
+
+            <?php $objets = ObjetDAL::findAll(); ?>
+            <div class="row">
+                <div class="col-lg-8">
+                    <legend>Liste des Objets précédemment ajoutés</legend>
+                    <table class="table table-bordered table-hover table-condensed">
+                        <thead>
+                            <tr>
+                                <th class="text-center">Label</th>
+                                <th class="text-center">Description</th>
+                                <th class="text-center">Modifier</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                            <?php foreach ($objets as $objet): ?>
+                                <tr>
+                                    <td class="text-center"><?php echo $objet->getLabel(); ?></td>
+                                    <td class="text-center"><?php echo $objet->getDescription(); ?></td>
+                                    <td class="text-center"><a href=<?php $_SERVER['DOCUMENT_ROOT'] ?>"/view/phtml/mod_unObjet.php?idObjet=<?php echo $objet->getId(); ?>" class="btn btn-primary btn-sm active">Mod</a></td> <!-- Lien vers une page view qui affiche les détail (permet leur modif) -->
+                                </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
+
+                <br><br>
+
+                <div class="panel panel-danger col-lg-4">
+                    <div class="panel-heading">
+                        <h3 class="panel-title"><b>ATTENTION: Suppression d'Objets</b></h3>
+                    </div>
+                    <div class="panel-body text-justify text-danger">
+                        Attention, la suppression d'un Objet est impossible car cela peut entrainer des erreurs par la suite.<br>
+                    </div>
+                </div>
+            </div>
+            
         </div>
-        
-        
-        
     </body>
 </html>
